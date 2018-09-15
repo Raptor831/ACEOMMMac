fe7450811856676613ff27c59134e46a20c03c15
Raptor831 <Raptor831@users.noreply.github.com>
2018-09-15 16:19:39 -0400

    Add base XML data processing

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
-<template>
-  <h2>Hello from {{text}}</h2>
-</template>
-
 <script>
+import xml2js from 'xml2js'
+import fs from 'fs';
+
 export default {
   name: 'app',
   data () {
     return {
        message: '',
-       text: ''
+       text: '',
+       data: '',
     }
   },
   created () {
@@ -16,10 +16,13 @@ export default {
   },
   methods: {
     refreshMessage() {
-      this.$http.get('./message.json').then( response => {
-        this.text = response.data.fizz;
-        this.message = response.data;
-        //console.log(this.message);
+      let parseString = xml2js.parseString
+      fs.readFile(__dirname + '/../data/Datastore.xml', (err, data) => {
+        xml2js.parseString(data, (err, result) => {
+          this.data = result;
+          this.text = result.Data.Businesses[0].Airlines[0].Airline[0]['$'].Name
+          console.log('Done');
+        });
       });
     }
   }
